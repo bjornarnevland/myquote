ba7b9fd51c7aad412599fa66ba946bb30e057989
bjornarnevland <bjormn16@uia.no>
2022-01-27 11:05:37 +0100

    modul1

---
+++ myquote.go
@@ -0,0 +1,23 @@
+
+
+package myquote
+
+import "rsc.io/quote"
+
+func MyGlass() string {
+	myGlass := quote.Glass()
+	return myGlass
+func MyGo() string {
+	return quote.Go()
+}
+
+func MyHello() string {
+	return quote.Hello()
+}
+
+func MyOpt() string {
+	return quote.Opt()
+
+}
+
+
